1d047d27f824b8560cd077eca6606b927946a3e2
Victoria Sun <victoria.s.sun@gmail.com>
2018-02-01 18:13:24 -0800

    close listener

--- src/components/lightbox.js
+++ src/components/lightbox.js
@@ -1,14 +1,22 @@
 class Lightbox {
   constructor(onClose) {
+    this.isOpen = false;
     this.element = document.getElementById('lightbox');
     this.back = this.element.getElementsByClassName('back')[0];
     this.forward = this.element.getElementsByClassName('forward')[0];
     this.title = this.element.getElementsByClassName('title')[0];
     this.img = this.element.getElementsByTagName('img')[0];
     this.onCloseLightbox = onClose;
+
+    document.addEventListener('keydown', event => {
+      if (event.key === 'Escape' || event.keyCode === 27) {
+        this.close();
+      }
+    });
   }
 
   show(imgUrl, title, onClickNext = null, onClickBack = null) {
+    this.open = true;
     this.element.className = '';
     this.img.src = imgUrl;
     this.title.innerHTML = title;
@@ -16,23 +24,28 @@ class Lightbox {
 
     window.requestAnimationFrame(t => {
       window.requestAnimationFrame(t2 => {
-        this.element.style.visibility = 'visible';
+        this.element.style.display = 'block';
         this.element.className = 'fade-in';
       });
     });
   }
 
   close() {
-    this.element.className = '';
+    if (this.open) {
+      this.element.className = '';
 
-    window.requestAnimationFrame(t => {
-      window.requestAnimationFrame(t2 => {
-        this.element.style.visibility = 'hidden';
-        this.element.className = 'fade-out';
+      window.requestAnimationFrame(t => {
+        window.requestAnimationFrame(t2 => {
+          this.element.className = 'fade-out';
+
+          setTimeout(() => {
+            this.element.style.display = 'none';
+          }, 299);
+        });
       });
-    });
 
-    this.onCloseLightbox();
+      this.onCloseLightbox();
+    }
   }
 }
 
